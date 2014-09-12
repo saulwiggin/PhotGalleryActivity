800544d0c467e829d5e53355c591f1fa4a76b174
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 14:39:14 +0100

    create model object

---
+++ app/src/main/java/GalleryItem/GalleryItem.java
@@ -0,0 +1,14 @@
+package GalleryItem;
+
+/**
+ * Created by Icarus on 9/12/2014.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    PRIVATE sTRING MuRl;
+
+    public String toString() {
+        return mCaption;
+    }
+}
