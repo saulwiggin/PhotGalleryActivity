c939cccec1d3d69088117e229057959c86d28718
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 15:53:19 +0100

    implement setup adapter add adpater update code create galleryitemadapter initial thread code create thumbnaildownloader hook thumbnaildownloader obtaining sending and handling a message add response handler hook up to reponse handler downloading and diplaying add cleanup method call cleanup method add flickr search method hardwired search query code add search menu items Add search strings options menu callbacks options menu callbacks

--- app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
@@ -6,6 +6,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -67,6 +70,25 @@ private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>>
         protected Void doInBackground(Void... params) {
             new FlikrFetchr().fetchItems
             return null;
+        }
+
+        @Override
+        public void onCreateOptionMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.fragment_photo_gallery, menu);
+        }
+
+        @override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.menu_item_search:
+                    getActivity().onSearchRequested();
+                    return true;
+                case R.id.menu_item_clear:
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
         }
 
         @Override
