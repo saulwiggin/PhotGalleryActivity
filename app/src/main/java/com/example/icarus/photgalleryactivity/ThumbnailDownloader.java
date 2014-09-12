b174dbfab097f417a9175363f1f95ef8300f6d52
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 15:17:01 +0100

    implement setup adapter add adpater update code create galleryitemadapter initial thread code create thumbnaildownloader hook thumbnaildownloader obtaining sending and handling a message

--- app/src/main/java/com/example/icarus/photgalleryactivity/ThumbnailDownloader.java
+++ app/src/main/java/com/example/icarus/photgalleryactivity/ThumbnailDownloader.java
@@ -1,19 +1,72 @@
 package com.example.icarus.photgalleryactivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Handler;
+
+import FlickerFetchr.FlickrFetchr;
 
 /**
  * Created by Icarus on 9/12/2014.
  */
 public class ThumbnailDownloader<Token> extends HandlerThread {
-    private static final String TAG = "Thumbnaildown";
+    private static final String TAG = "Thumbnaildown";private static final int MESSAGE_DOWNLOAD = 0;
+
+    Handler mHandler;
+    Map<Token, String> requestMap =
+            Collections.synchronized(new HashMap<Token, String>());
 
     public ThumbnailDownloader(){
         super(TAG);
     }
 
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onLooperPrepared() {
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    @suppressWarnings("unchecked")
+                    Token token = (token)msg.ibj;
+                    Log.i(TAG, "Got a request for url: " + requestMap.get(token));
+                    handleRequest(token);
+                }
+            }
+        };
+    }
+
     public void queueThumbnail(Token token, String url) {
         Log.i(TAG, "Got an URL: " + url);
+        requestMap.put(token, url);
+
+        mHandler
+                .obtainMessage(MESSAGE_DOWNLOAD, token){
+                .sentToTarget();
+        }
+
+        private void handleRequest(final Token token) {
+        try {
+            final String url = requestMap.get(token);
+            if (url == null)
+                return;
+
+            byte[] butmapBytes = new FlickrFetchr().getUrkBytes(url);
+            final Bitmap bitmap = BitmapFactory
+                    .decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+        } catch (IOException ioe){
+            Log.e(TAG, "Error downloading image", ioe);
+        }
+    }
     }
 }
