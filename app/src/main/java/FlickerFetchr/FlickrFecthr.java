866834843c1b8ec393dadcfdbdacddb249f7296d
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 09:07:52 +0100

    Basic networking code

---
+++ app/src/main/java/FlickerFetchr/FlickrFecthr.java
@@ -0,0 +1,40 @@
+package FlickerFetchr;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Icarus on 9/12/2014.
+ */
+public class FlickrFecthr {
+    byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlspec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection)url.openConnetion();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResoinseCode() != HttpURLConnection.HTTP_OK) {
+                return null;
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.writte(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+    public String getURL(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+}
