5490dac6d12c373e9588e09ba07d6df21bb54fc5
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 14:54:30 +0100

    implement setup adapter add adpater update code

--- app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
@@ -11,17 +11,17 @@ import android.widget.GridView;
 
 import java.io.IOException;
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
-        new FlickrFetchr().fetchItems();
-        return null;
-    }
-
-    private class FetchItemsTask extneds AsyncTask<Void,Void,Void> {
+    private class FetchItemsTask extneds AsyncTask<Void,Void,ArrayList<GalleryItem>> {
         @Override
-            protected Void doInBackground(Void... params){
-        new FlickrFetchr().fetchItems();
-        return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {}
+
+            new FlickrFetchr().fetchItems();
+
         }
+
+        @override protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
 
     GridView mGridView;
@@ -42,42 +42,41 @@ import java.io.IOException;
             return null;
 
         }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_photo_gallery);
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
-
-        mGridView = (GridView) v.findViewById(R.id.gridView);
-
-
-        setupAdapter();
-
-
-        return v;
-    }
-
-    void setupAdapter() {
-        if (getActivity() == null || mGridView == null) return;
-
-        if (mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
-                    android.R.layout.simple_gallery_item, mItems));
-        } else {
-            mGridView.setAdapter(null);
-        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_photo_gallery);
         }
 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+            mGridView = (GridView) v.findViewById(R.id.gridView);
 
 
+            setupAdapter();
 
 
-    @Override
-    public Fragment createFragment() {
-        return new PhotGalleryFragment();
-}
+            return v;
+        }
+
+        void setupAdapter() {
+            if (getActivity() == null || mGridView == null) return;
+
+            if (mItems != null) {
+                mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+                        android.R.layout.simple_gallery_item, mItems));
+            } else {
+                mGridView.setAdapter(null);
+            }
+        }
+
+
+        @Override
+        public Fragment createFragment() {
+            return new PhotGalleryFragment();
+        }
+    }
