c0a8f7fe15bece75f3e3d0bb1fda8cebf9512333
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-12 09:13:51 +0100

    Add networking permission to manifest writing an asynctask

--- app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/icarus/photgalleryactivity/PhotoGalleryActivity.java
@@ -1,7 +1,9 @@
 package com.example.icarus.photgalleryactivity;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,25 @@ import android.widget.GridView;
 
 
 public class PhotoGalleryActivity extends Fragment {
+    private static final String TAG = "PhotGalleryFragment";
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+
+            }
+            return null;
+        }
+    }
+
+}
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
