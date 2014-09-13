c79b8b51ea6575c21cd4becee038a78e2a853360
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-13 09:15:20 +0100

    implement setup adapter add adpater update code create galleryitemadapter initial thread code create thumbnaildownloader hook thumbnaildownloader obtaining sending and handling a message add response handler hook up to reponse handler downloading and diplaying add cleanup method call cleanup method add flickr search method hardwired search query code add search menu items Add search strings options menu callbacks options menu callbacks search configuration add intent filter and metadata startup receivers adding your receiver to the manifest add alarm status preference

---
+++ app/src/main/java/Pollservice.java
@@ -0,0 +1,54 @@
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Icarus on 9/13/2014.
+ */
+public class Pollservice extends IntentService {
+    private static final String TAG = "Pollservice";
+
+    private static final String PREF_IS_ALARM_ON = "isAlarmOn";
+
+    public PollService() {
+        super(TAG);
+    }
+
+    public static void setService Alarm(Context context, boolean isOn) {
+        Intent i = new Intent(context, Pollservice.class);
+
+        private static final int POLL_INTERVAL = 1000 + 60 * 5 ; // 5 miinutes
+        public static final String PREF_IS ALARM_ON = "isAlaramOn";
+
+        public Pollservice() {
+            super(TAG);
+        }
+
+        public static void setServiceAlarm(Conext context, boolean isOn) {
+        Intent i = new Intent(context, Pollservice.class);
+        PendingIntent pi = PendingIntent.getService(
+                context, 0, i, 0);
+
+        AlarmManager alaramMangager = (AlarmManger)
+                context.getSystemsService(Context.ALARM_SERVICE);
+    } else
+
+    {
+        alarmManger.setRepeating(AlarmManager.RTC,
+                System.currentTimeMillis(), POLL_INTERVAL, pi);
+    } else {
+        alarmManager.cancel(pi);
+        pi.cancel();
+    }
+    PreferenceManager.getDefaultShardedPreferences(context)
+    .edit()
+        .putBoolean(PollService.PREF_IS_ALARM_ON,isOn)
+        .commit();
+    }
+        )
+    }
+    }
+}
